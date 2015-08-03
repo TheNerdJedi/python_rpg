dd0388c06a18ed8757cd2e3c84fe56f6794be926
Shajed Ahmed <shajedahmed@gmail.com>
2015-08-03 12:40:29 -0400

    Created Class Types
    :bicyclist: :horse_racing: :snowboarder: :swimmer: :surfer:
    :mountain_bicyclist:

--- rpg.py
+++ rpg.py
@@ -51,7 +51,7 @@ class Character:
 	def defend(self):
 		pass
 
-	
+
 class Player(Character):
 	"""Create Player Class"""
 	# initialize player at level 1 with 0 experience
@@ -91,3 +91,42 @@ class Player(Character):
 				self.level_up()
 				self.levels.remove(value)
 				break
+
+	# take in enemies defeated from battle and add exp to total player controlled character
+
+
+
+
+	# create different class types
+class Fighter(Player):
+	"""Defines Fighter Class"""
+	def __init__(self, name):
+		self.name = name
+		self.class_type = "Fighter"
+		self.weapon = "sword"
+		self.inventory = []
+		self.stats = {'BASE_HP': 12, 'BASE_MP': 2, 'BASE_ATK': 10, 'BASE_DFS': 5, 'BASE_MAGIC_ATK': 2, 'BASE_SPD': 4, 'BASE_LUCK': 6}
+		self.modifiers = {'BASE_HP': 4, 'BASE_MP': 1, 'BASE_ATK': 2, 'BASE_DFS': 3, 'BASE_MAGIC_ATK': 2, 'BASE_SPD': 1, 'BASE_LUCK': 1}
+		self.stats_init()
+
+class Mage(Player):
+	"""Defines Mage Class"""
+	def __init__(self, name):
+		self.name = name
+		self.class_type = "Mage"
+		self.weapon = "wand"
+		self.inventory = []
+		self.stats = {'BASE_HP': 10, 'BASE_MP': 8, 'BASE_ATK': 5, 'BASE_DFS': 4, 'BASE_MAGIC_ATK': 12, 'BASE_SPD': 7, 'BASE_LUCK': 5}
+		self.modifiers = {'BASE_HP': 2, 'BASE_MP': 4, 'BASE_ATK': 1, 'BASE_DFS': 3, 'BASE_MAGIC_ATK': 3, 'BASE_SPD': 2, 'BASE_LUCK': 1}
+		self.stats_init()
+		
+class Rogue(Player):
+	"""Defines Rogue Class"""
+	def __init__(self, name):
+		self.name = name
+		self.class_type = "Rogue"
+		self.weapon = "dagger"
+		self.inventory = []
+		self.stats = {'BASE_HP': 10, 'BASE_MP': 4, 'BASE_ATK': 7, 'BASE_DFS': 4, 'BASE_MAGIC_ATK': 7, 'BASE_SPD': 10, 'BASE_LUCK': 10}
+		self.modifiers = {'BASE_HP': 2, 'BASE_MP': 2, 'BASE_ATK': 2, 'BASE_DFS': 1, 'BASE_MAGIC_ATK': 2, 'BASE_SPD': 3, 'BASE_LUCK': 3}
+		self.stats_init()
